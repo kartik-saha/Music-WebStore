35b525a08f9728b11e01870cfda38636c069e02f
Akarsh <akarsh.khandelwal_cs22@gla.ac.in>
2025-03-28 20:19:30 +0530

    Small changes

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,17 @@
-// const express = require("express");
-// const cors = require("cors");
-// require("dotenv").config();
-// const connectDB = require("./config/db");
-// const authRoutes = require("./routes/auth"); // Import Auth Routes
-
-// const app = express();
-
-// // Middleware
-// app.use(express.json());
-// app.use(cors());
-
-// // Connect to MongoDB Atlas
-// connectDB();
-
-// // Routes
-// app.use("/api/auth", authRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config({ path: "./.env" });
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+connectDB();
+console.log("MongoDB URI:", process.env.MONGO_URI);
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello from Express Backend!" });
