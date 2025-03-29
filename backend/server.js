42636abe09b9176faacaa5627a86e0f8389b012a
singhabhinendra <singhabhinendra337@gmail.com>
2025-03-29 17:06:48 +0530

    remove some typos

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,20 @@
-<<<<<<< HEAD
+
 const express=require("express");
 const connectDB =require("./config/db");
 require("dotenv").config();
 const app= express();
 const PORT = process.env.PORT || 5000;
-=======
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config({ path: "./.env" });
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
->>>>>>> 611ead8e5935e6de9c80ef122c01828d9f16cb68
 
 connectDB();
 
-<<<<<<< HEAD
 app.get ("/",(req,res)=>{
     res.send("API is running...")
-=======
+});
 connectDB();
 console.log("MongoDB URI:", process.env.MONGO_URI);
 
@@ -25,7 +22,6 @@ app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello from Express Backend!" });
->>>>>>> 611ead8e5935e6de9c80ef122c01828d9f16cb68
 });
 
 app.listen (PORT,()=>{
